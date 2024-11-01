f261269b67216c4e17c279131644233cc0da2351
mohammad khajehnoori <mohammad.khajehnoori.05@gmail.com>
2024-11-01 23:31:04 +0330

    the answer of Q2 was added + making the code more clear

--- task_1/src/task_1/Q2.java
+++ task_1/src/task_1/Q2.java
@@ -3,5 +3,18 @@ package task_1;
 
  
 public class Q2 {
+     
+        public static boolean checkParity(String code) { 
+    
+        int count = 0;
+    
+        for (int i = 0; i < code.length(); i++){ 
+            if (code.charAt(i) == '1'){ 
+                count++; 
+            } 
+        } 
+        
+        return count % 2 == 0; 
+    }
     
 }
--- task_1/src/task_1/Task_1.java
+++ task_1/src/task_1/Task_1.java
@@ -4,11 +4,28 @@ public class Task_1 {
 
  
     public static void main(String[] args) {
+        System.out.println("\n <first Question> \n");
+    // ================================== #1 ==============================================
       String[] codes = { "0110011001001100", "0001110111011100", "0000000011110000" }; 
       
       for (String code : codes){ 
           Q1.checkHammingCode(code); 
       }
+      
+    // ================================================================================
+    System.out.print("\n <second Question> \n");
+    // ==================================== #2 ============================================
+    
+    String code = "0110011001001100"; 
+    if (Q2.checkParity(code)) { 
+        System.out.println("The code has even parity."); 
+    } 
+    else { 
+        System.out.println("The code has odd parity."); 
     }
+    }
+    
+    // ================================================================================
+    // ==================================== #3 ============================================
     
 }
