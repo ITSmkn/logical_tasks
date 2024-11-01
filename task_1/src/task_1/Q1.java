9b8c720c3e70a41d2db3b7c78adad76a568d0d19
mohammad khajehnoori <mohammad.khajehnoori.05@gmail.com>
2024-11-01 23:21:58 +0330

    completing the answer of the first question

--- task_1/src/task_1/Q1.java
+++ task_1/src/task_1/Q1.java
@@ -1,7 +1,38 @@
- 
 package task_1;
 
- 
+
 public class Q1 {
+    public static int calculateParity(int[] bits, int pos){ int parity = 0; 
+    for (int i = 0; i < bits.length; i++){ 
+        if ((i + 1 & pos) != 0){ 
+            parity ^= bits[i];
+    } 
+    } 
+     return parity;
+}
     
+    public static void checkHammingCode(String code){ 
+        int[] bits = new int[code.length()]; 
+        
+        for (int i = 0; i < code.length(); i++) { bits[i] = Character.getNumericValue(code.charAt(i)); } 
+        
+        int[] parityBits = {1, 2, 4, 8, 16}; int errorPosition = 0; 
+        
+        for (int parity : parityBits) { int parityCheck = calculateParity(bits, parity); errorPosition += parityCheck * parity; } 
+        if (errorPosition == 0){ 
+            System.out.println("No error detected for code: " + code); 
+        } 
+        else { 
+            System.out.println("Error detected at position " + errorPosition + " for code: " + code); 
+        
+            bits[errorPosition - 1] ^= 1; 
+        
+            System.out.print("Corrected code: "); 
+        
+            for (int bit : bits){ 
+                System.out.print(bit); 
+        } 
+            System.out.println(); 
+    } // end of else ... 
+    }
 }
--- task_1/src/task_1/Task_1.java
+++ task_1/src/task_1/Task_1.java
@@ -4,7 +4,11 @@ public class Task_1 {
 
  
     public static void main(String[] args) {
+      String[] codes = { "0110011001001100", "0001110111011100", "0000000011110000" }; 
       
+      for (String code : codes){ 
+          Q1.checkHammingCode(code); 
+      }
     }
     
 }
